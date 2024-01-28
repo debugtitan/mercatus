38b67a798fb17a30de723af59135ae5db75f06b4
debugtitan <alts.devs@gmail.com>
2024-01-28 21:39:43 +0100

    remove unsupported native date picker

--- app/screens/auth/Signup.js
+++ app/screens/auth/Signup.js
@@ -3,11 +3,13 @@ import React, { useState } from "react";
 import PageLayout from "../../PageLayout";
 import { useTheme } from "../../helpers/ThemeProvider";
 import { DARK, LIGHT, STYLES, RoutePaths } from "../../constants";
-
+import DatePicker from "react-native-date-picker";
 export default function ({ navigation }) {
   const { isDarkMode } = useTheme();
   const theme = isDarkMode ? DARK : LIGHT;
   const styles = STYLES();
+  const [date, setDate] = useState(new Date());
+  const [open, setOpen] = useState(false);
   return (
     <PageLayout>
       <View className="mt-5 flex-1">
@@ -17,8 +19,8 @@ export default function ({ navigation }) {
           <View style={styles.textInputContainer}>
             <TextInput
               placeholder="Enter Name"
-              inputMode='text'
-              keyboardType='name-phone-pad'
+              inputMode="text"
+              keyboardType="name-phone-pad"
               style={styles.textInput}
               cursorColor={theme.TABS_INACTIVE}
               placeholderTextColor={theme.TABS_INACTIVE}
@@ -41,9 +43,10 @@ export default function ({ navigation }) {
           </View>
         </View>
 
-   
-      
-       
+        {/*DOB */}
+
+        
+
         {/*BUTTON */}
         <View className="mt-8">
           <View className="my-5">
@@ -57,8 +60,6 @@ export default function ({ navigation }) {
               </Text>
             </TouchableOpacity>
           </View>
-
-          
         </View>
       </View>
     </PageLayout>
