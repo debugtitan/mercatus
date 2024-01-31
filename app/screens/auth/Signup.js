b97aaaf684f07c4601c6b3c46fe42fead5f0c9bb
debugtitan <alts.devs@gmail.com>
2024-01-31 19:42:40 +0100

    refactoring codes with  prototype

--- app/screens/auth/Signup.js
+++ app/screens/auth/Signup.js
@@ -1,18 +1,51 @@
-import { View, Text, TextInput, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Platform,
+} from "react-native";
 import React, { useState } from "react";
 import PageLayout from "../../PageLayout";
 import { useTheme } from "../../helpers/ThemeProvider";
 import { DARK, LIGHT, STYLES, RoutePaths } from "../../constants";
-import DatePicker from "react-native-date-picker";
+import DateTimePicker from "@react-native-community/datetimepicker";
+
 export default function ({ navigation }) {
   const { isDarkMode } = useTheme();
   const theme = isDarkMode ? DARK : LIGHT;
   const styles = STYLES();
+
+  const [dob, setDob] = useState("");
+  const [gender, setGender] = useState("");
+
   const [date, setDate] = useState(new Date());
   const [open, setOpen] = useState(false);
+
+  const onChange = ({ type }, selectedDate) => {
+    const currentDate = selectedDate;
+    if (type == "set") {
+      setDate(currentDate);
+
+      if (Platform.OS === "android") {
+        toggleDatePicker();
+        setDob(currentDate.toDateString());
+      }
+    } else {
+      toggleDatePicker();
+    }
+  };
+
+  const toggleDatePicker = () => {
+    setOpen((prev) => !prev);
+  };
+
+  console.log(gender)
+
+  
   return (
     <PageLayout>
-      <View className="mt-5 flex-1">
+      <View className="mt-2 flex-1">
         {/* NAME */}
         <View className="mb-4">
           <Text style={styles.textLabel}>Name</Text>
@@ -44,8 +77,65 @@ export default function ({ navigation }) {
         </View>
 
         {/*DOB */}
+        {open && (
+          <DateTimePicker
+            value={date}
+            mode="date"
+            display="spinner"
+            onChange={onChange}
+          />
+        )}
+        <View className="mb-4">
+          <Text style={styles.textLabel}>Date of birth</Text>
+          <TouchableOpacity onPress={toggleDatePicker}>
+            <View style={styles.textInputContainer}>
+              <TextInput
+                placeholder="DD/MM/YY"
+                style={styles.textInput}
+                value={dob}
+                cursorColor={theme.TABS_INACTIVE}
+                placeholderTextColor={theme.TABS_INACTIVE}
+                editable={false}
+              />
+            </View>
+          </TouchableOpacity>
+        </View>
 
-        
+        {/* GENDER */}
+        <View className="mb-4">
+          <Text style={styles.textLabel}>Gender</Text>
+          <TouchableOpacity>
+            <View style={styles.textInputContainer}>
+              <TextInput
+                placeholder="M or F"
+                inputMode="text"
+                style={styles.textInput}
+                cursorColor={theme.TABS_INACTIVE}
+                onChange={(e) => setGender(e.target.value)}
+                placeholderTextColor={theme.TABS_INACTIVE}
+                maxLength={1}
+              />
+            </View>
+          </TouchableOpacity>
+        </View>
+
+        {/* Country */}
+
+        <View className="mb-4">
+          <TouchableOpacity>
+            <Text style={styles.textLabel}>Country</Text>
+
+            <View style={styles.textInputContainer}>
+              <View style={styles.textInput}>
+                
+              </View>
+            </View>
+          </TouchableOpacity>
+        </View>
+
+        {/* CHECKBOX */}
+        <View className="mb-5">
+        </View>
 
         {/*BUTTON */}
         <View className="mt-8">
