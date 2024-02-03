ba6a5f4d4d3e98e0c3a61539847afe685f013aa5
debugtitan <alts.devs@gmail.com>
2024-02-03 18:44:19 +0100

    country list

--- app/components/CountryRadioPicker.js
+++ app/components/CountryRadioPicker.js
@@ -1,10 +1,48 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
 
 export default function CountryRadioPicker() {
+  const countries = [
+    {
+      label: "Nigeria",
+      flag: require("../../assets/icons/flags/ng.png"),
+    },
+    {
+      label: "Angola",
+      flag: require("../../assets/icons/flags/ng.png"),
+    },
+    {
+      label: "Botswana",
+      flag: require("../../assets/icons/flags/ng.png"),
+    },
+    {
+      label: "Egypt",
+      flag: require("../../assets/icons/flags/ng.png"),
+    },
+    {
+      label: "Libya",
+      flag: require("../../assets/icons/flags/ng.png"),
+    },
+    {
+      label: "Namibia",
+      flag: require("../../assets/icons/flags/ng.png"),
+    },
+    {
+      label: "South Sudan",
+      flag: require("../../assets/icons/flags/ng.png"),
+    },
+    {
+      label: "Zambia",
+      flag: require("../../assets/icons/flags/ng.png"),
+    },
+    {
+      label: "Zimbabwe",
+      flag: require("../../assets/icons/flags/ng.png"),
+    },
+  ];
   return (
     <View>
       <Text>CountryRadioPicker</Text>
     </View>
-  )
+  );
 }
