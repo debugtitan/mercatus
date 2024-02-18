b745751d29d4b80276cda555c148cd2b92b8923f
debugtitan <alts.devs@gmail.com>
2024-02-18 14:32:36 +0100

    finally achieved immersive systemnavigation bar using kotlin

--- App.tsx
+++ App.tsx
@@ -6,11 +6,11 @@
  */
 
 import React, { useEffect } from 'react';
-import SystemNavigationBar from 'react-native-system-navigation-bar';
+//import SystemNavigationBar from 'react-native-system-navigation-bar';
 import SplashScreen from 'react-native-splash-screen';
 import { ThemeProvider } from './components';
 import { MercatusStackNavigator } from './screens/StackNavigationComponent';
-SystemNavigationBar.navigationHide();
+//SystemNavigationBar.navigationHide();
 export default function App(): React.JSX.Element {
   useEffect(() => {
     SplashScreen.hide();
--- android/app/src/main/java/com/mercatus/MainActivity.kt
+++ android/app/src/main/java/com/mercatus/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.mercatus
 import android.os.Bundle;
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import androidx.core.view.WindowCompat
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
@@ -8,6 +11,27 @@ import org.devio.rn.splashscreen.SplashScreen;
 
 class MainActivity : ReactActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
+    val windowInsetsController =
+        WindowCompat.getInsetsController(window, window.decorView)
+    // Configure the behavior of the hidden system bars.
+    windowInsetsController.systemBarsBehavior =
+        WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+
+    window.decorView.setOnApplyWindowInsetsListener { view, windowInsets ->
+        // You can hide the caption bar even when the other system bars are visible.
+        // To account for this, explicitly check the visibility of navigationBars()
+        // and statusBars() rather than checking the visibility of systemBars().
+        if (windowInsets.isVisible(WindowInsetsCompat.Type.navigationBars())) {
+                // Hide both the status bar and the navigation bar.
+                windowInsetsController.hide(WindowInsetsCompat.Type.navigationBars())
+        }
+        else {
+                windowInsetsController.show(WindowInsetsCompat.Type.navigationBars())
+        }
+        
+        view.onApplyWindowInsets(windowInsets)
+      }
+
     SplashScreen.show(this) // here
     super.onCreate(savedInstanceState)
 }
